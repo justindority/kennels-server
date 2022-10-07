7dcfbcacf67ac77687e47a330481746dffed328e
Justin Dority <justindority@gmail.com>
2022-10-07 10:09:45 -0500

    explorer chapters

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,16 +1,21 @@
+from .location_requests import get_single_location
+from .customer_requests import get_single_customer
+
 ANIMALS = [
     {
         "id": 1,
         "name": "Snickers",
         "species": "Dog",
         "locationId": 1,
-        "customerId": 4
+        "status": "Admitted",
+        "customerId": 1
     },
     {
         "id": 2,
         "name": "Roman",
         "species": "Dog",
         "locationId": 1,
+        "Status": "Admitted",
         "customerId": 2
     },
     {
@@ -18,10 +23,93 @@ ANIMALS = [
         "name": "Blue",
         "species": "Cat",
         "locationId": 2,
+        "Status": "Admitted",
         "customerId": 1
     }
 ]
 
 
+    # Function with a single parameter
+def get_single_animal(id):
+    # Variable to hold the found animal, if it exists
+    requested_animal = None
+
+    # Iterate the ANIMALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for animal in ANIMALS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if animal["id"] == id:
+            requested_animal = animal
+    
+    if requested_animal == None:
+        return None
+
+
+    location = get_single_location(requested_animal["locationId"])
+    customer = get_single_customer(requested_animal["customerId"])
+    requested_animal["customer"] = customer
+    requested_animal["location"] = location
+    requested_animal.pop("customerId")
+    requested_animal.pop("locationId")
+    return requested_animal
+
 def get_all_animals():
     return ANIMALS
+
+def create_animal(animal):
+
+    if "name" not in animal:
+        return "Name is required"
+
+    if "species" not in animal:
+        return "Species is required"
+
+    if "locationId" not in animal:
+        return "locationId is required"
+    
+    if "Status" not in animal:
+        return "Status is required"
+    
+    if "customerId" not in animal:
+        return "customerId is required"
+
+    # Get the id value of the last animal in the list
+    max_id = ANIMALS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    animal["id"] = new_id
+
+    # Add the animal dictionary to the list
+    ANIMALS.append(animal)
+
+    # Return the dictionary with `id` property added
+    return animal
+
+
+def delete_animal(id):
+    # Initial -1 value for animal index, in case one isn't found
+    animal_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Store the current index.
+            animal_index = index
+
+    # If the animal was found, use pop(int) to remove it from list
+    if animal_index >= 0:
+        ANIMALS.pop(animal_index)
+
+def update_animal(id, new_animal):
+    # Iterate the ANIMALS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Update the value.
+            ANIMALS[index] = new_animal
+            break
